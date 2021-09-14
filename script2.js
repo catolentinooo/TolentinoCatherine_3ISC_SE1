bb18b2142d80f186eed1303d2ed81d645dd95790
Catherine Tolentino <marycatherine.tolentino.iics@ust.edu.ph>
2021-09-14 09:51:15 +0800

    added script2.js

---
+++ script2.js
@@ -0,0 +1,6 @@
+function search(array,item){
+    return array.indexOf(item)
+};
+
+console.log(search(['gta','valorant','csgo'],'valorant'));
+console.log(search(['green','yellow','blue'],'pink'));
